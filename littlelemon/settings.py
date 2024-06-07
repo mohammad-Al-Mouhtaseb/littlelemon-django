2066a77860a6f632e9ec41368a3041225f554895
Mohammad Al Mouhtaseb <83731873+mohammad-Al-Mouhtaseb@users.noreply.github.com>
2024-06-07 07:46:40 +0300

    Update settings.py

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -76,10 +76,21 @@ WSGI_APPLICATION = 'littlelemon.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'bFqxdHXtrDjqKwzOHTievEGowzureXpV',
+        'HOST': 'monorail.proxy.rlwy.net',
+        'PORT': '45004',
     }
 }
 
